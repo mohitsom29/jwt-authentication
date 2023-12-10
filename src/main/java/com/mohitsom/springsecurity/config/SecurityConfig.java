838dc025082d012a5b2a568ad02a1fbf09e8a3c9
Mohit Somvanshi <coder@Mohits-MacBook-Air.local>
2023-12-10 14:46:03 +0530

    Upgraded to spring boot version '3.2.0' & edit the SecurityConfig file

--- src/main/java/com/mohitsom/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/mohitsom/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,85 +20,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         http
-                 .csrf()
-                 .disable()
-                 .authorizeHttpRequests()
-                 .requestMatchers("/api/v1/auth/**")
-                 .permitAll()
-                 .anyRequest()
-                 .authenticated()
-                 .and()
-                 .sessionManagement()
-                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                 .and()
-                 .authenticationProvider(authenticationProvider)
-                 .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class);
+         http.csrf(AbstractHttpConfigurer::disable)
+                 .cors(AbstractHttpConfigurer::disable)
+                 .authorizeHttpRequests(
+                         auth-> auth.requestMatchers("/api/v1/auth/**").authenticated()
+                                 .requestMatchers("/api/v1/auth/**").permitAll()
+                                 .anyRequest().authenticated())
+
+                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider);
+         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
          return http.build();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
